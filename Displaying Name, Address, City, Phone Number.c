d0ac8ab2c275508eadc790e8aedc6036e6a99934
Lijo Jolly <lijojolly10@gmail.com>
2024-03-09 23:11:07 +0530

    Add files via upload

---
+++ Displaying Name, Address, City, Phone Number.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("NAME: Lijo Jolly\n");
+    printf("ADDRESS: Thiruvampady, Alappuzha, Kerala\n");
+    printf("CITY: Alappuzha\n");
+    printf("PHONE: 6666666666\n");
+}
